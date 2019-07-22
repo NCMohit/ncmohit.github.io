e01e1167be374153bbf042805a8d43d10fb18bbe
N C Mohit <44508697+NCMohit@users.noreply.github.com>
2019-07-22 11:58:52 +0530

    Update popup.js

--- test/js/popup.js
+++ test/js/popup.js
@@ -25,5 +25,73 @@ $(function()
       $('#colorbox').css('background-color', color);
       $('#colorbox2').css('background-color', color);
     });
+    $("#b1").click(function(){     
+      if ($("#i2").hasClass("iup")) {
+        document.getElementById("b2").click();
+      }
+      if ($("#i3").hasClass("iup")) {
+        document.getElementById("b3").click();
+      }
+      if ($("#i4").hasClass("iup")) {
+        document.getElementById("b4").click();
+      }
+      if ($("#i1").hasClass("iup")) {
+        $("#i1").toggleClass("idown iup");
+      }
+      else{
+        $("#i1").toggleClass("idown iup");
+      }
+    });
+    $("#b2").click(function(){
+      if ($("#i1").hasClass("iup")) {
+        document.getElementById("b1").click();
+      }
+      if ($("#i3").hasClass("iup")) {
+        document.getElementById("b3").click();
+      }
+      if ($("#i4").hasClass("iup")) {
+        document.getElementById("b4").click();
+      }
+      if ($("#i2").hasClass("iup")) {
+        $("#i2").toggleClass("idown iup");
+      }
+      else{
+        $("#i2").toggleClass("iup idown");
+      }
+    });
+    $("#b3").click(function(){
+      if ($("#i1").hasClass("iup")) {
+        document.getElementById("b1").click();
+      }
+      if ($("#i2").hasClass("iup")) {
+        document.getElementById("b2").click();
+      }
+      if ($("#i4").hasClass("iup")) {
+        document.getElementById("b4").click();
+      }
+      if ($("#i3").hasClass("iup")) {
+        $("#i3").toggleClass("idown iup");
+      }
+      else{
+        $("#i3").toggleClass("iup idown");
+      }
+    });
+    $("#b4").click(function(){
+      if ($("#i1").hasClass("iup")) {
+        document.getElementById("b1").click();
+      }
+      if ($("#i2").hasClass("iup")) {
+        document.getElementById("b2").click();
+      }
+      if ($("#i3").hasClass("iup")) {
+        document.getElementById("b3").click();
+      }
+      if ($("#i4").hasClass("iup")) {
+        $("#i4").toggleClass("idown iup");
+      }
+      else{
+        $("#i4").toggleClass("iup idown");
+      }
+    });
 });
 
