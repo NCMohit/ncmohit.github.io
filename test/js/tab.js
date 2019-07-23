6db2db4440eb4ec1f176795f5cdf4864bc24b0f0
N C Mohit <44508697+NCMohit@users.noreply.github.com>
2019-07-23 12:25:36 +0530

    Update tab.js

--- test/js/tab.js
+++ test/js/tab.js
@@ -26,6 +26,33 @@ function removeeffect(){
   document.getElementById("effectmain").classList.remove("shine");
   document.getElementById("effectmain").innerHTML = "None";
 }
+function addicon1fb(){
+  document.getElementById("icon1").innerHTML = "<i class='zmdi zmdi-facebook-box' style='float: left;'></i>Facebook";
+}
+function addicon1em(){
+  document.getElementById("icon1").innerHTML = "<i class='zmdi zmdi-email' style='float: left;'></i>E-Mail";
+}
+function addicon1ws(){
+  document.getElementById("icon1").innerHTML = "<i id='test1' class='zmdi zmdi-whatsapp' style='float: left;'></i>Whatsapp";
+}
+function addicon2fb(){
+  document.getElementById("icon2").innerHTML = "<i class='zmdi zmdi-facebook-box' style='float: left;'></i>Facebook";
+}
+function addicon2em(){
+  document.getElementById("icon2").innerHTML = "<i class='zmdi zmdi-email' style='float: left;'></i>E-Mail";
+}
+function addicon2ws(){
+  document.getElementById("icon2").innerHTML = "<i id='test1' class='zmdi zmdi-whatsapp' style='float: left;'></i>Whatsapp";
+}
+function addicon3fb(){
+  document.getElementById("icon3").innerHTML = "<i class='zmdi zmdi-facebook-box' style='float: left;'></i>Facebook";
+}
+function addicon3em(){
+  document.getElementById("icon3").innerHTML = "<i class='zmdi zmdi-email' style='float: left;'></i>E-Mail";
+}
+function addicon3ws(){
+  document.getElementById("icon3").innerHTML = "<i id='test1' class='zmdi zmdi-whatsapp' style='float: left;'></i>Whatsapp";
+}
 document.getElementById("defaultopen").click();
 document.getElementById("b1").click();
 document.getElementById("b5").click();
