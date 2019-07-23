06dd4dd342d237bce682bfe100f4d7110310840c
N C Mohit <44508697+NCMohit@users.noreply.github.com>
2019-07-23 13:08:58 +0530

    Update tab.js

--- test/js/tab.js
+++ test/js/tab.js
@@ -1,12 +1,12 @@
 function openbar(evt, cityName) {
-  var i, tabcontent, tablinks;
+  var p, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+  for (p = 0; p < tabcontent.length; p++) {
+    tabcontent[p].style.display = "none";
   }
   tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  for (p = 0; p < tablinks.length; p++) {
+    tablinks[p].className = tablinks[p].className.replace(" active", "");
   }
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
@@ -27,31 +27,31 @@ function removeeffect(){
   document.getElementById("effectmain").innerHTML = "None";
 }
 function addicon1fb(){
-  document.getElementById("icon1").innerHTML = "<i class='zmdi zmdi-facebook-box' style='float: left;'></i>Facebook";
+  document.getElementById("icon1").innerHTML = "<p class='zmdi zmdi-facebook-box' style='float: left;'></p>Facebook";
 }
 function addicon1em(){
-  document.getElementById("icon1").innerHTML = "<i class='zmdi zmdi-email' style='float: left;'></i>E-Mail";
+  document.getElementById("icon1").innerHTML = "<p class='zmdi zmdi-email' style='float: left;'></p>E-Mail";
 }
 function addicon1ws(){
-  document.getElementById("icon1").innerHTML = "<i id='test1' class='zmdi zmdi-whatsapp' style='float: left;'></i>Whatsapp";
+  document.getElementById("icon1").innerHTML = "<p id='test1' class='zmdi zmdi-whatsapp' style='float: left;'></p>Whatsapp";
 }
 function addicon2fb(){
-  document.getElementById("icon2").innerHTML = "<i class='zmdi zmdi-facebook-box' style='float: left;'></i>Facebook";
+  document.getElementById("icon2").innerHTML = "<p class='zmdi zmdi-facebook-box' style='float: left;'></p>Facebook";
 }
 function addicon2em(){
-  document.getElementById("icon2").innerHTML = "<i class='zmdi zmdi-email' style='float: left;'></i>E-Mail";
+  document.getElementById("icon2").innerHTML = "<p class='zmdi zmdi-email' style='float: left;'></p>E-Mail";
 }
 function addicon2ws(){
-  document.getElementById("icon2").innerHTML = "<i id='test1' class='zmdi zmdi-whatsapp' style='float: left;'></i>Whatsapp";
+  document.getElementById("icon2").innerHTML = "<p id='test1' class='zmdi zmdi-whatsapp' style='float: left;'></p>Whatsapp";
 }
 function addicon3fb(){
-  document.getElementById("icon3").innerHTML = "<i class='zmdi zmdi-facebook-box' style='float: left;'></i>Facebook";
+  document.getElementById("icon3").innerHTML = "<p class='zmdi zmdi-facebook-box' style='float: left;'></p>Facebook";
 }
 function addicon3em(){
-  document.getElementById("icon3").innerHTML = "<i class='zmdi zmdi-email' style='float: left;'></i>E-Mail";
+  document.getElementById("icon3").innerHTML = "<p class='zmdi zmdi-email' style='float: left;'></p>E-Mail";
 }
 function addicon3ws(){
-  document.getElementById("icon3").innerHTML = "<i id='test1' class='zmdi zmdi-whatsapp' style='float: left;'></i>Whatsapp";
+  document.getElementById("icon3").innerHTML = "<p id='test1' class='zmdi zmdi-whatsapp' style='float: left;'></p>Whatsapp";
 }
 document.getElementById("defaultopen").click();
 document.getElementById("b1").click();
